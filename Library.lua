8cfa4e7486b766eb388e02ab1b80177cd6674afb
TrapstarKSSKSKSKKS <101758758+TrapstarKSSKSKSKKS@users.noreply.github.com>
2024-01-16 01:33:15 -0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,4 +1,4 @@
-print("Loading Linoria UI v2.29.9")
+print("Loading Linoria UI v2.29.10")
 
 -- violin-suzutsuki i love you !!!!!!
 
@@ -3926,7 +3926,7 @@ function Library:CreateWindow(...)
 
 		local FadeTime = Config.MenuFadeTime
 		Library.Fading = true
-		Library.Toggled = not Toggled
+		Library.Toggled = not not Library.Toggled -- If it's true, make it false. If it's false, make it true.
 		warn(Library.Toggled, "Ui Toggled")
 		ModalElement.Modal = Toggled
 
